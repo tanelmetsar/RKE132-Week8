3c50e104b34b2528ac21513ced37eec718a26a7c
tanelmetsar <tanel.metsar@tktk.ee>
2024-04-26 23:02:54 +0300

    add week8 code

---
+++ Week8/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] heroes = { "Harry Potter", "Luke Skywalker", "Lara Croft", "Scooby-Doo" };
+string[] villains = { "Voldemort", "Darth Vader", "Dracula", "Joker", "Sauron" };
+string[] weapon = { "magic wand", "plastic fork", "banana", "wooden sword", "Lego brick" };
+
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} saves the day!");
+
+string villain = GetRandomValueFromArray(villains);
+Console.WriteLine($"Today {villain} tries to take over the world!");
+
+
+
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Lenght);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
+
+    
